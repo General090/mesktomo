9a74b06ff8cfe9418e8432ef664ccff2fdf77c7d
General090 <gospelkalu33@gmail.com>
2025-10-13 12:39:01 +0100

    updated

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,30 +1,93 @@
-import React from 'react'
-import Logo from "../assets/mesklogo.png"
+import React from "react";
+import Logo from "../assets/mesklogo.png";
+import { MdLocalPhone } from "react-icons/md";
+import { CiMail } from "react-icons/ci";
+import { BsTwitterX } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
 
-const layout = ({ children }) => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <nav className='flex items-center justify-between mt-5 px-30 font-semibold'>
-        <div>
-            <img src={Logo} className='w-18' />
-        </div>
+      <nav className="fixed top-0 left-0 w-full z-50 overflow-x-hidden transition-colors ease-in-out duration-300 bg-[#00000080] shadow-md text-white py-5">
+        <div className="flex items-center justify-between px-30 font-semibold">
+          <div>
+            <img src={Logo} alt="MESKTOMO Logo" className="w-15" />
+          </div>
 
-        <div className='flex items-center gap-10 list-none'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Blogs</li>
-            <li>Contact</li>
-            <li>Services</li>
-        </div>
+          <ul className="hidden md:flex items-center gap-10 list-none">
+            <li className="hover:text-yellow-400 cursor-pointer">Home</li>
+            <li className="hover:text-yellow-400 cursor-pointer">About</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Blogs</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Contact</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Services</li>
+          </ul>
 
-        <button className='bg-[#ddd53b] rounded-full w-50 py-3 text-black'>Get a Free Quote</button>
+          <button className="bg-[#ddd53b] text-black font-semibold rounded-full px-6 py-3 hover:bg-white transition">
+            Get a Free Quote
+          </button>
+        </div>
       </nav>
 
       <main>{children}</main>
 
-      <footer></footer>
-    </div>
-  )
-}
+      <footer className="bg-[#0f2c27] text-white text-sm py-15 px-30 flex justify-between">
+        <div className="space-y-5">
+          <img src={Logo} className="w-15" />
+          <p className="w-55">
+            Efficient, time-bound logistics that impacts your business growth |
+            Delivering Excellence in Logistics Solutions.
+          </p>
+        </div>
 
-export default layout
+        <div className="space-y-5">
+          <h1 className="font-bold text-xl">Quick Links</h1>
+          <ul className="space-y-5">
+            <li className="hover:text-yellow-400 cursor-pointer">Home</li>
+            <li className="hover:text-yellow-400 cursor-pointer">About</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Blogs</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Contact</li>
+            <li className="hover:text-yellow-400 cursor-pointer">Services</li>
+          </ul>
+        </div>
+
+        <div className="space-y-5">
+          <h1 className="font-bold text-xl">Contact</h1>
+          <div className="flex items-center gap-2">
+            <MdLocalPhone className="text-[#c3c039] text-lg" />
+            08140807725
+          </div>
+
+          <div className="flex items-center gap-2">
+            <MdLocalPhone className="text-[#c3c039] text-lg" />
+            08140807725
+          </div>
+
+          <div className="flex items-center gap-2">
+            <MdLocalPhone className="text-[#c3c039] text-lg" />
+            08140807725
+          </div>
+
+          <div className="flex items-center gap-2">
+            <CiMail className="text-[#c3c039] text-lg" />
+            sales@mesktomolog.com
+          </div>
+
+          <div className="flex items-center gap-2">
+            <MdLocalPhone className="text-[#c3c039] text-lg" />
+            25 Diya Street, Gbagada, Lagos, Nigeria.
+          </div>
+        </div>
+
+        <div className="space-y-5">
+          <h1 className="font-bold text-xl">Follow us</h1>
+          <div className="flex items-center gap-5">
+           <BsTwitterX className="text-lg" />
+           <FiInstagram className="text-lg" />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../../layout";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="relative background h-screen flex flex-col items-center justify-center text-center text-white pt-20">
+        <div className="relative z-10 px-6 animate-slideUp">
+          <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
+            Delivering Excellence <br /> in Logistics Solutions
+          </h1>
+          <p className="text-lg md:text-[15px] max-w-2xl mx-auto mb-5">
+            As an industry leader, we specialize in providing comprehensive
+            logistics solutions designed to connect businesses, facilitate
+            trade, and empower growth across regional and international markets.
+          </p>
+          <button
+            className="bg-[#ddd53b] text-black font-semibold px-6 py-3 rounded-full hover:bg-white transform transition-all duration-300 hover:scale-105"
+          >
+            Get a Free Quote
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
